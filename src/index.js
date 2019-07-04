7b8ba85b210b23e7f5c84b69a96d709a81664cb1
JavaScript Joe <jjprevite@gmail.com>
2019-07-03 18:32:08 -0700

    Add useHello hook

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+
+export function useHello (person, dependencies = []) {
+  useEffect(() => {
+    console.log(`Hello, ${person}`)
+  },dependencies)
+}
 
 const HelloComponent = () => <div> Hello World! </div>
 
