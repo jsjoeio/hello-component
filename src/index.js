8ff894efb019b3f6ad20861285005aed81fd6532
JavaScript Joe <jjprevite@gmail.com>
2019-07-03 19:28:07 -0700

    Add yalc for local development

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
 
 export default function useHello (person, dependencies = []) {
-  useEffect(() => {
+  React.useEffect(() => {
     console.log(`Hello, ${person}`)
   },dependencies)
 }
